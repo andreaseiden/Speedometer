5e76e5c943d9a8c4b799d306a56b13d410632f8c
Andreas Eiden <andreas.eiden@gmx.de>
2024-07-31 15:02:02 +0200

    2. Versuch

--- Speedometer/ContentView.swift
+++ Speedometer/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, Speedometer!")
+            Text("Hello, Speedometer! Jetzt in Github")
         }
         .padding()
     }
