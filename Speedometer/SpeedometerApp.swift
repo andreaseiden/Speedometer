9de4614ef30ea1e58e9805722f7aadaa3d151214
Andreas Eiden <andreas.eiden@gmx.de>
2024-07-31 13:54:29 +0200

    Initial Commit

---
+++ Speedometer/SpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerApp.swift
+//  Speedometer
+//
+//  Created by Andreas Eiden on 31.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
